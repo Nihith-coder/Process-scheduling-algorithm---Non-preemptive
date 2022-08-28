e7f7b79a56633f6b8c4fd6c0e5e448171336bbe1
Nihith <89455995+Nihith-coder@users.noreply.github.com>
2022-08-28 11:42:24 +0530

    Create data.h

---
+++ data.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <cmath>
+#include <vector>
+#include<cstdlib>
+using namespace std;
+ 
+typedef struct node
+{
+    int pri;
+    int pid;
+    int bt;
+    int wt;
+    int tat;
+    int at;
+    int st;
+    int ct;
+} Node;
